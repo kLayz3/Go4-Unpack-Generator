66963ed1b459bccc601f03be996d004756a549cc
kLayz3 <bajzekm@gmail.com>
2023-09-10 21:23:13 +0200

    rnd

--- proc_macro_lib/src/lib.rs
+++ proc_macro_lib/src/lib.rs
@@ -1,9 +1,21 @@
 extern crate proc_macro;
-use proc_macro::TokenStream;
 
-#[proc_macro]
-pub fn make_answer(_item: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+use proc_macro::TokenStream;
+use regex::Regex;
+use std::process::Command;
+
+macro_rules! exec_command {
+    ($com_name: ident $($arg:tt)*) => {
+        String::from_utf8(
+            Command::new(stringify!($com_name))
+            $(
+            .arg(stringify!($arg))
+            )*
+            .output()
+            .unwrap()
+            .stdout).unwrap();
+    };
+    () => {};
 }
 
 #[proc_macro]
@@ -17,15 +29,46 @@ pub fn load_spec_file(_item: TokenStream) -> TokenStream {
 }
 #[proc_macro]
 pub fn load_headers(_item: TokenStream) -> TokenStream {
+    let mut head: TokenStream = "munch!()".parse().unwrap();
+
+    let re = Regex::new(r##"^#include[ \t\n]+"([\w\-/\.]+)"$"##).unwrap(); 
     let s = std::fs::read_to_string("event.spec").unwrap();
-    let re = Regex::new(r##"^#include[ \t]+"([\w/\.]+)"$"##); 
+    
+    let mut pwd = Command::new("pwd");
+    let mut curr_dir = exec_command!(pwd);
+
+    let pwd_dir: String = String::from_utf8(Command::new("ls").arg("../").output().unwrap().stdout).unwrap(); 
+
+    println!("Curr_dir : {}", curr_dir);
+    println!("With PWD: {}", pwd_dir);
+
+    pwd.current_dir("../");
+    curr_dir = get_result(&mut pwd);
+    println!("Curr pwd dir : {}", curr_dir); 
+
+    // Find all occurences of `include` in the file:
+    // Do this iteratively. In case includes have other includes,
+    // until all `include` are resolved, while keeping track of possible
+    // cross referencing. Perform DFS and parse the terminal nodes
+    // first. Current path should always be inside the 
+    let mut it = re.captures_iter(&s);
+    let mut files_seen: Vec<String> = vec![];
+    while let Some(caps) = it.next() {
+        files_seen.push(caps[1].to_owned());
+        // Process includes inside the capture:
+        process_file(&caps[1], &mut pwd, &mut files_seen);
+    }
+
+    head
 }
 
-#[proc_macro]
-pub fn proc_test_lex(_item: TokenStream) -> TokenStream {
-    let mut head = "test_lex!".parse::<TokenStream>().unwrap(); 
-    let end = "(kekega)".parse::<TokenStream>().unwrap();
 
-    head.extend(end);
-    head
+#[inline]
+fn get_result(command: &mut Command) -> String {
+    String::from_utf8(command.output().unwrap().stdout).unwrap()
+}
+
+fn process_file(file_name: &str, file_dir: &mut Command, f_stack: &mut Vec<String>) {
+    let s = std::fs::read_to_string(file_name);
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 extern crate proc_macro_lib;
 extern crate decl_macro_lib;
-use proc_macro_lib::make_answer;
 use proc_macro_lib::load_spec_file;
-use proc_macro_lib::proc_test_lex;
+use proc_macro_lib::load_headers;
 
 use decl_macro_lib::formatt;
 use decl_macro_lib::munch;
-use decl_macro_lib::test_lex;
-
-use regex::Regex;
 
 #[allow(unused_imports)] use std::fs::File;
 #[allow(unused_imports)] use std::io::{self, prelude::*, BufReader};
@@ -21,12 +17,9 @@ pub static __KGRN:  &str = "\x1B[32m";
 pub static __KCYN:  &str = "\x1B[36m";
 
 
-make_answer!();
-
 fn main() -> std::io::Result<()> {
-    load_spec_file!();
-
     load_headers!();
+    load_spec_file!();
     
     println!("Hello, world!");
 
